6833db65e92faec5608da14ef6e426c50484c958
Jenyajk <101403726+Jenyajk@users.noreply.github.com>
2022-05-23 12:12:15 +0500

    feat: add task-04

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -1,21 +1,20 @@
 const fs = require('fs');
 const process = require('process');
+const file = '02-write-file/file.txt';
 
-fs.writeFile('02-write-file/file.txt','a', (err) => {
+fs.writeFile(file,'a', (err) => {
   if (err) {
     console.error(err);
   }
 });
 
-process.stdin.on('data', data => fs.appendFile('02-write-file/file.txt', data , (err) => {
+process.stdin.on('data', data => fs.appendFile(file, data , (err) => {
   if (err) {
     console.error(err);
   }
 
 }));
 
-process.on('exit', () => {
-  console.log('Process exit event with code');
-});
-
+process.on('SIGINT', () => process.exit());
+process.on('exit', () => process.stdout.write('Bye'));
 console.log('Write for me');
--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,15 @@
+const fs = require('fs/promises');
+const path = require('path');
+const folderPath = '04-copy-directory/files';
+const folderPathCopy = '04-copy-directory/files-copy';
 
+async function copyDir() {
+  await fs.rm(folderPathCopy, { recursive: true, force: true });
+  await fs.mkdir(folderPathCopy);
+  await fs.readdir(folderPath)
+    .then(files => {
+      files.forEach(file => fs.copyFile(path.join(folderPath, file), path.join(folderPathCopy, file)));
+    });
+}
+
+copyDir();
