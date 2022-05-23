4c03996871ba9f65cd8fae6a1ae84946beb436ec
Jenyajk <101403726+Jenyajk@users.noreply.github.com>
2022-05-23 16:55:31 +0500

    feat: add task-05

---
+++ 05-merge-styles/index.js
@@ -0,0 +1,54 @@
+const fs = require('fs');
+const path = require('path');
+const folderPathStyle = '05-merge-styles/styles';
+const folderPath = '05-merge-styles/project-dist';
+fs.writeFile(path.join(folderPath, 'bundle.css'),'', (err) => {
+  if (err) {
+    console.error(err);
+  }
+});
+fs.readdir(folderPathStyle, (err, files) => {
+  if (err) {
+    console.error(err);
+  }
+  files.forEach(file => {
+    let pop = path.extname(file);
+    if (pop !== '.css'){
+      const noCss = path.basename(file);
+      const noCssPath = path.join(folderPathStyle, noCss);
+      fs.unlink(noCssPath, (err) => {
+        if (err) {
+          console.error(err);
+        }
+      });
+    }
+  });
+  const output = fs.createWriteStream(path.join(folderPath, 'bundle.css'));
+  if (files.length === 1) {
+    const input = fs.createReadStream(path.join(folderPathStyle, files[0]));
+    input.pipe(output);}
+  if (files.length === 2) {
+    const input = fs.createReadStream(path.join(folderPathStyle, files[0]));
+    const input1 = fs.createReadStream(path.join(folderPathStyle, files[1]));
+    input.pipe(output);
+    input1.pipe(output);}
+  if (files.length === 3) {
+    const input = fs.createReadStream(path.join(folderPathStyle, files[0]));
+    const input1 = fs.createReadStream(path.join(folderPathStyle, files[1]));
+    const input2 = fs.createReadStream(path.join(folderPathStyle, files[2]));
+    input.pipe(output);
+    input1.pipe(output);
+    input2.pipe(output);}
+  if (files.length === 4) {
+    const input = fs.createReadStream(path.join(folderPathStyle, files[0]));
+    const input1 = fs.createReadStream(path.join(folderPathStyle, files[1]));
+    const input2 = fs.createReadStream(path.join(folderPathStyle, files[2]));
+    const input3 = fs.createReadStream(path.join(folderPathStyle, files[3]));
+    input.pipe(output);
+    input1.pipe(output);
+    input2.pipe(output);
+    input3.pipe(output);}
+});
+
+
+
