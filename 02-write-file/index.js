d7f5fb8916409c66e21b83441b12d8bde358fb32
Jenyajk <101403726+Jenyajk@users.noreply.github.com>
2022-05-24 23:29:22 +0500

    feat: add if()

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -9,15 +9,20 @@ fs.writeFile(file,'', (err) => {
   process.stdout.write('Write for me\n');
 });
 
-process.stdin.on('data', data => fs.appendFile(file, data , (err) => {
-  if (err) {
-    console.error(err);
-  }
-
-}));
+process.stdin.on('data', data => {
+  if ((data.toString().trim() === 'exit')) {
+    process.stdout.write('Bye!');
+    process.exit();
+  } else {fs.appendFile(file, data , (err) => {
+    if (err) {
+      console.error(err);
+    }
+  });}
+}
+);
 
 process.on('SIGINT', () => {
-  process.stdout.write('Bye');
   process.exit();
 });
+
 
