3bae212affc38f919d412b47ab71178314793dd3
Jenyajk <101403726+Jenyajk@users.noreply.github.com>
2022-05-24 19:53:08 +0500

    fix: debug func

--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -2,47 +2,58 @@ const fs = require('fs');
 const path = require('path');
 const folderPathStyle = '05-merge-styles/styles';
 const folderPath = '05-merge-styles/project-dist';
+const test =[];
 
-fs.readdir(folderPathStyle, (err, files) => {
-  if (err) {
-    console.error(err);
-  }
-  files.forEach(file => {
-    let pop = path.extname(file);
-    if (pop !== '.css'){
-      const noCss = path.basename(file);
-      const noCssPath = path.join(folderPathStyle, noCss);
-      fs.unlink(noCssPath, (err) => {
-        if (err) {
-          console.error('CDCSCDD' + err);
+
+async function Merge(){
+  try {
+    fs.readdir(folderPathStyle, (err, files) => {
+      if (err) {
+        console.error(err);
+      }
+      files.forEach(file => {
+        let pop = path.extname(file);
+        if (pop === '.css'){
+          test.push(file);
+          test.flat();
+          return test;
+
         }
       });
-    }
-  });
-  const output = fs.createWriteStream(path.join(folderPath, 'bundle.css'));
-  if (files.length === 1) {
-    const input = fs.createReadStream(path.join(folderPathStyle, files[0]));
-    input.pipe(output);}
-  if (files.length === 2) {
-    const input = fs.createReadStream(path.join(folderPathStyle, files[0]));
-    const input1 = fs.createReadStream(path.join(folderPathStyle, files[1]));
-    input.pipe(output);
-    input1.pipe(output);}
-  if (files.length === 3) {
-    const input = fs.createReadStream(path.join(folderPathStyle, files[0]));
-    const input1 = fs.createReadStream(path.join(folderPathStyle, files[1]));
-    const input2 = fs.createReadStream(path.join(folderPathStyle, files[2]));
-    input.pipe(output);
-    input1.pipe(output);
-    input2.pipe(output);}
-  if (files.length === 4) {
-    const input = fs.createReadStream(path.join(folderPathStyle, files[0]));
-    const input1 = fs.createReadStream(path.join(folderPathStyle, files[1]));
-    const input2 = fs.createReadStream(path.join(folderPathStyle, files[2]));
-    const input3 = fs.createReadStream(path.join(folderPathStyle, files[3]));
-    input.pipe(output);
-    input1.pipe(output);
-    input2.pipe(output);
-    input3.pipe(output);}
-});
+      const output = fs.createWriteStream(path.join(folderPath, 'bundle.css'));
+      if (test.length === 1) {
+        const input = fs.createReadStream(path.join(folderPathStyle, test[0]));
+        input.pipe(output);}
+      if (test.length === 2) {
+        const input = fs.createReadStream(path.join(folderPathStyle, test[0]));
+        const input1 = fs.createReadStream(path.join(folderPathStyle, test[1]));
+        input.pipe(output);
+        input1.pipe(output);}
+      if (test.length === 3) {
+        const input = fs.createReadStream(path.join(folderPathStyle, test[0]));
+        const input1 = fs.createReadStream(path.join(folderPathStyle, test[1]));
+        const input2 = fs.createReadStream(path.join(folderPathStyle, test[2]));
+        input.pipe(output);
+        input1.pipe(output);
+        input2.pipe(output);}
+      if (test.length === 4) {
+        const input = fs.createReadStream(path.join(folderPathStyle, test[0]));
+        const input1 = fs.createReadStream(path.join(folderPathStyle, test[1]));
+        const input2 = fs.createReadStream(path.join(folderPathStyle, test[2]));
+        const input3 = fs.createReadStream(path.join(folderPathStyle, test[3]));
+        input.pipe(output);
+        input1.pipe(output);
+        input2.pipe(output);
+        input3.pipe(output);}
+    });
+
+
+  } catch (err) {
+    console.error( 'Файл не создан' + err);
+  }
+}
+
+Merge();
+
+
 
