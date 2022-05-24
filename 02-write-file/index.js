a2485801f14eedcf527cabeb50af591bd3f269be
Jenyajk <101403726+Jenyajk@users.noreply.github.com>
2022-05-24 20:21:44 +0500

    fix: exit bug

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 const process = require('process');
 const file = '02-write-file/file.txt';
 
-fs.writeFile(file,'a', (err) => {
+fs.writeFile(file,'', (err) => {
   if (err) {
     console.error(err);
   }
@@ -15,6 +15,8 @@ process.stdin.on('data', data => fs.appendFile(file, data , (err) => {
 
 }));
 
-process.on('SIGINT', () => process.exit());
-process.on('exit', () => process.stdout.write('Bye'));
+process.on('SIGINT', () => {
+  process.stdout.write('Bye');
+  process.exit();
+});
 console.log('Write for me');
